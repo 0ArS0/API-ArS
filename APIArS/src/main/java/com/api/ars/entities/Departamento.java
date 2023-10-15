2035915211503c2da8d3bf15bfc5e92d30cb7fa0
ArthurM <arthurmonteiro20172018@gmail.com>
2023-10-15 16:57:13 -0300

    feat: Declaração dos atributos e relacionamento entre as entidades

--- APIArS/src/main/java/com/api/ars/entities/Departamento.java
+++ APIArS/src/main/java/com/api/ars/entities/Departamento.java
@@ -1,5 +1,8 @@
 package com.api.ars.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,30 +10,131 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_departamento")
+@Table(name = "tb_dept")
 public class Departamento {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_dept")
 	private Integer id;
+	
+	@Column(name = "nome_dept")
+	private String nome;
+	
+	@Column(name = "endereco_dept")
+	private String endereco;
+	
+	@Column(name = "email_dept")
+	private String email;
+	
+	@Column(name = "telefone_fixo_dept")
+	private String telefoneFixo;
+	
+	@Column(name = "celular_dept")
+	private String celular;
+	
+	@Column(name = "descricao_dept")
+	private String descricao;
+	
+	@Column(name = "data_criacao_dept")
+	private LocalDate dataCriacao;
+	
+	@Column(name = "orcamento_dept")
+	private Double orcamento;
 
-	/*
-	 * Nome do Departamento
-	 * 
-	 * Descrição
-	 * 
-	 * Data de Criação
-	 * 
-	 * Funcionários
-	 * 
-	 * Orçamento
-	 * 
-	 * Endereço
-	 * 
-	 * Telefone
-	 * 
-	 * E-mail
-	 */
-	
-	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTelefoneFixo() {
+		return telefoneFixo;
+	}
+
+	public void setTelefoneFixo(String telefoneFixo) {
+		this.telefoneFixo = telefoneFixo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Double getOrcamento() {
+		return orcamento;
+	}
+
+	public void setOrcamento(Double orcamento) {
+		this.orcamento = orcamento;
+	}
+
+	public Departamento() {
+	}
+
+	public Departamento(Integer id, String nome, String descricao, String telefoneFixo, String endereco, String email,
+			String celular, LocalDate dataCriacao, Double orcamento) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.telefoneFixo = telefoneFixo;
+		this.endereco = endereco;
+		this.email = email;
+		this.celular = celular;
+		this.dataCriacao = dataCriacao;
+		this.orcamento = orcamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", telefoneFixo="
+				+ telefoneFixo + ", endereco=" + endereco + ", email=" + email + ", telefoneCelular=" + celular
+				+ ", DataCriacao=" + dataCriacao + ", orcamento=" + orcamento + "]";
+	}
+
 }
--- APIArS/src/main/java/com/api/ars/entities/Funcionario.java
+++ APIArS/src/main/java/com/api/ars/entities/Funcionario.java
@@ -1,39 +1,171 @@
 package com.api.ars.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_funcionario")
+@Table(name = "tb_func")
 public class Funcionario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_func")
 	private Integer id;
+	
+	@Column(name = "nome_func")
+	private String nome;
+	
+	@Column(name = "endereco_func")
+	private String endereco;
+	
+	@Column(name = "email_func")
+	private String email;
+	
+	@Column(name = "telefone_fixo_func")
+	private String telefoneFixo;
+	
+	@Column(name = "celular_func")
+	private String celular;
+	
+	@Column(name = "genero_func")
+	private String genero;
+	
+	@Column(name = "cargo_func")
+	private String cargo;
+	
+	@Column(name = "salario_func")
+	private Double salario;
+	
+	@Column(name = "data_contratacao_func")
+	private LocalDate dataContratacao;
+	
+	@Column(name = "data_nascimento_func")
+	private LocalDate dataNascimento;
+	
+	@ManyToOne
+	@JoinColumn(name = "dept_id")
+	private Departamento departamento;
+	
+	public Integer getId() {
+		return id;
+	}
 
-	/*
-	 * Nome Completo
-	 * 
-	 * Data de Nascimento
-	 * 
-	 * Gênero
-	 * 
-	 * Cargo
-	 * 
-	 * Departamento
-	 * 
-	 * Data de Contratação
-	 * 
-	 * Salário
-	 * 
-	 * Endereço
-	 * 
-	 * Telefone
-	 * 
-	 * E-mail
-	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefoneFixo() {
+		return telefoneFixo;
+	}
+
+	public void setTelefoneFixo(String telefoneFixo) {
+		this.telefoneFixo = telefoneFixo;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public LocalDate getDataContratacao() {
+		return dataContratacao;
+	}
+
+	public void setDataContratacao(LocalDate dataContratacao) {
+		this.dataContratacao = dataContratacao;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Funcionario() {
+	}
+
+	public Funcionario(Integer id, String nome, String genero, String cargo, String endereco, String telefoneFixo,
+			String celular, String email, Double salario, LocalDate dataContratacao, LocalDate dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.genero = genero;
+		this.cargo = cargo;
+		this.endereco = endereco;
+		this.telefoneFixo = telefoneFixo;
+		this.celular = celular;
+		this.email = email;
+		this.salario = salario;
+		this.dataContratacao = dataContratacao;
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [id=" + id + ", nome=" + nome + ", genero=" + genero + ", Cargo=" + cargo + ", endereco="
+				+ endereco + ", telefoneFixo=" + telefoneFixo + ", celular=" + celular + ", email=" + email
+				+ ", salario=" + salario + ", dataContratacao=" + dataContratacao + ", dataNascimento=" + dataNascimento
+				+ "]";
+	}
+	
 }
