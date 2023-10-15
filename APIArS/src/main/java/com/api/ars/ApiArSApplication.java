0ac84e6a72dbdb5573be546ec900cd638a8bcf15
ArthurM <arthurmonteiro20172018@gmail.com>
2023-10-15 14:43:25 -0300

    Initial commit

---
+++ APIArS/src/main/java/com/api/ars/ApiArSApplication.java
@@ -0,0 +1,13 @@
+package com.api.ars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiArSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiArSApplication.class, args);
+	}
+
+}
