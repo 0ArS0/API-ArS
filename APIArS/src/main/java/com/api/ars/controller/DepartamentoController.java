80da4ec8c359141463913443d602399b0aff364d
ArthurM <arthurmonteiro20172018@gmail.com>
2023-10-15 21:35:26 -0300

    feat: Implementando CRUD simples nos controladores de Departamento e Funcionario

--- APIArS/src/main/java/com/api/ars/controller/DepartamentoController.java
+++ APIArS/src/main/java/com/api/ars/controller/DepartamentoController.java
@@ -1,10 +1,49 @@
 package com.api.ars.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.ars.entities.Departamento;
+import com.api.ars.services.DepartamentoService;
 
 @RestController
 @RequestMapping("/departamento")
 public class DepartamentoController {
+	
+	@Autowired
+	DepartamentoService departamentoService;
+	
+	@GetMapping("/buscar/{id}")
+	public Departamento buscarPorId(@PathVariable Integer id) {
+		return departamentoService.buscarPorId(id);
+	}
 
+	@GetMapping("/listar")
+	public List<Departamento> listarTodos() {
+		return departamentoService.listarTodos();
+	}
+	
+	@PostMapping("/salvar")
+	public Departamento salvar(@RequestBody Departamento departamento) {
+		return departamentoService.salvar(departamento);
+	}
+	
+	@PutMapping("/atualizar/{id}")
+	public Departamento atualizar(@PathVariable Integer id, @RequestBody Departamento departamento) {
+		return departamentoService.atualizar(id, departamento);
+	}
+	
+	@DeleteMapping("/remover/{id}")
+	public void removerLogico(@PathVariable Integer id) {
+		departamentoService.removerLogico(id);
+	}
 }
--- APIArS/src/main/java/com/api/ars/controller/FuncionarioController.java
+++ APIArS/src/main/java/com/api/ars/controller/FuncionarioController.java
@@ -1,10 +1,50 @@
 package com.api.ars.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.ars.entities.Funcionario;
+import com.api.ars.services.FuncionarioService;
 
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
+	
+	@Autowired
+	FuncionarioService funcionarioService;
+	
+	@GetMapping("/buscar/{id}")
+	public Funcionario buscarPorId(@PathVariable Integer id) {
+		return funcionarioService.buscarPorId(id);
+	}
 
+	@GetMapping("/listar")
+	public List<Funcionario> listarTodos() {
+		return funcionarioService.listarTodos();
+	}
+	
+	@PostMapping("/salvar")
+	public Funcionario salvar(@RequestBody Funcionario funcionario) {
+		return funcionarioService.salvar(funcionario);
+
+	}
+	
+	@PutMapping("/atualizar/{id}")
+	public Funcionario atualizar(@PathVariable Integer id, @RequestBody Funcionario funcionario) {
+		return funcionarioService.atualizar(id, funcionario);
+	}
+	
+	@DeleteMapping("/remover/{id}")
+	public void removerLogico(@PathVariable Integer id) {
+		funcionarioService.removerLogico(id);
+	}
 }
